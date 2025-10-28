1588b1137103f7314f096154999672e51267473a
Austin Horning <austinseven@outlook.com>
2025-10-28 13:28:28 -0700

    set camera height

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+const WINDOW_HEIGHT: f32 = 1600.0;
+
 // Plugin initializer for startup systems
 pub fn init_startup_systems(app: &mut App) {
     app.add_systems(Startup, setup_camera);
@@ -7,5 +9,12 @@ pub fn init_startup_systems(app: &mut App) {
 
 // Setup camera (runs once at startup)
 pub fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2d);
+
+
+    commands.spawn((Camera2d, Projection::from(OrthographicProjection {
+        scaling_mode: bevy::camera::ScalingMode::FixedVertical {
+                viewport_height: WINDOW_HEIGHT,
+            },
+            ..OrthographicProjection::default_2d()
+    })));
 }
